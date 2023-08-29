632c25bb52793e6af90a51df4b3b715b75f34e48
dadak797 <dadak797@gmail.com>
2023-08-29 21:16:42 +0900

    5.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-import Circle from "./Circle";
-import { useState } from "react";
-import styled from "styled-components";
+import Router from "./Router";
 
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
 
 function App() {
   return (
-    <Container>
-      <H1>
-        Protected
-      </H1>
-    </Container>
+    <Router />
   );
 }
 
