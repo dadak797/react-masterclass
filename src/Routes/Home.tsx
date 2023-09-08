d959aa2c0d985784b64202f8e2c974912d4811ed
dadak797 <dadak797@gmail.com>
2023-09-08 22:30:59 +0900

    9.9

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -44,30 +44,55 @@ const Slider = styled.div`
 
 const Row = styled(motion.div)`
     display: grid;
-    gap: 10px;
+    gap: 5px;
     grid-template-columns: repeat(6, 1fr);
     position: absolute;
     width: 100%;
 `;
 
-const Box = styled(motion.div)`
+const Box = styled(motion.div)<{ $bgPhoto: string }>`
     background-color: white;
-    height: 200px;
-    color: red;
+    background-image: url(${(props) => props.$bgPhoto});
+    background-size: cover;
+    background-position: center center;
+    height: 160px;
     font-size: 66px;
+    &:first-child {
+        transform-origin: center left;
+    }
+    &:last-child {
+        transform-origin: center right;
+    }
 `;
 
 const rowVariants = {
     hidden: {
-        x: window.outerWidth + 10,
+        x: window.outerWidth + 5,
     },
     visible: {
         x: 0,
     },
     exit: {
-        x: -window.outerWidth - 10,
+        x: -window.outerWidth - 5,
     }
 };
+
+const boxVariants = {
+    normal: {
+        scale: 1,
+    },
+    hover: {
+        scale: 1.3,
+        y: -50,
+        transition: {
+            delay: 0.5,
+            duration: 0.3,
+            type: "tween"
+        }
+    }
+}
+
+const offset = 6;
 
 function Home() {
     const {data, isLoading} = useQuery<IGetMoviesResult>(
@@ -75,7 +100,17 @@ function Home() {
         getMovies
     );
     const [index, setIndex] = useState(0);
-    const increaseIndex = () => setIndex((prev) => prev + 1);
+    const [leaving, setLeaving] = useState(false);
+    const increaseIndex = () => {
+        if (data) {
+            if (leaving) return;
+            toggleLeaving();
+            const totalMovies = data.results.length;
+            const maxIndex = Math.floor(totalMovies / offset) - 1;
+            setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+        }
+    }
+    const toggleLeaving = () => setLeaving((prev) => !prev);
     return (
         <Wrapper>
             {isLoading ? (
@@ -87,7 +122,7 @@ function Home() {
                     <Overview>{data?.results[0].overview}</Overview>
                 </Banner>
                 <Slider>
-                    <AnimatePresence>
+                    <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
                         <Row
                             variants={rowVariants}
                             initial="hidden"
@@ -96,7 +131,18 @@ function Home() {
                             transition={{ type: "tween", duration: 1 }}
                             key={index}
                         >
-                            {[1, 2, 3, 4, 5, 6].map(i => <Box key={i}>{i}</Box>)}
+                            {data?.results
+                                .slice(offset * index + 1, offset * index + offset + 1)
+                                .map((movie) => (
+                                    <Box 
+                                        key={movie.id} 
+                                        whileHover="hover"
+                                        initial="normal"
+                                        variants={boxVariants}
+                                        transition={{ type: "tween" }}
+                                        $bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                                    />
+                                ))}
                         </Row>
                     </AnimatePresence>
                 </Slider>
